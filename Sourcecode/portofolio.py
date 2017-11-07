476367ca4ba4cfb526efd8f3c43eb93c66d233a2
athanasakosangelos@gmail.com <athanasakosangelos@gmail.com>
2017-11-07 23:10:22 +0000

    Added split index and base html template, json data and a lot of css.

--- Sourcecode/portofolio.py
+++ Sourcecode/portofolio.py
@@ -1,10 +1,41 @@
+import os
 from flask  import Flask, url_for, abort, request, render_template, json, flash, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
-# Web root
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s/blog.db' % os.getcwd()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Custom 401
+@app.errorhandler(401)
+def http_error_handler(error):
+        return render_template('401.html', error=error), error.code
+
+# Custom 404
+@app.errorhandler(404)
+def http_error_handler(error):
+    return render_template('404.html', error=error), error.code
+
 @app.route("/")
 def root():
         return render_template('index.html'), 200
+
+@app.route("/work")
+def work_index():
+        return render_template('work_index.html'), 200
+
+@app.route("/work/websites/<int:page>/")
+def work_websites_index(page):
+    with open("websites.json","r") as f:
+        websites = json.load(f)
+        return render_template('work_websites.html', page=page, websites=websites), 200
+
+@app.route("/world")
+def world_index():
+        return render_template('world_index.html'), 200
+
 
 
 if __name__ == "__main__":

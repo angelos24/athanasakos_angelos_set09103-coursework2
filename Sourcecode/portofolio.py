09d8cd2fa48b7f4385820883635c81ce9a4f762f
athanasakosangelos@gmail.com <athanasakosangelos@gmail.com>
2017-11-09 16:45:42 +0000

    Added initial blog functionality

--- Sourcecode/portofolio.py
+++ Sourcecode/portofolio.py
@@ -18,6 +18,21 @@ def http_error_handler(error):
 def http_error_handler(error):
     return render_template('404.html', error=error), error.code
 
+class Pages(db.Model):
+    __tablename__ = 'pages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(1000))
+    content = db.Column(db.BLOB)
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+    def __repr__(self):
+        return '<Pages : id=%r, title=%s, content=%s>' \
+              % (self.id, self.title, self.content)
+
 @app.route("/")
 def root():
         return render_template('index.html'), 200
@@ -41,7 +56,20 @@ def work_websites_index_showcase(page):
 
 @app.route("/world")
 def world_index():
-        return render_template('world_index.html'), 200
+    pages = db.session.query(Pages).all()
+    return render_template('world_index.html', pages=pages), 200
+
+@app.route('/world/new_post')
+def new_post():
+    return render_template('world_newpage.html')
+
+@app.route('/new-post/', methods=['POST'])
+def save_post():
+    page = Pages(title=request.form['title'],
+                 content=request.form['content'])
+    db.session.add(page)
+    db.session.commit()
+    return redirect('world')
 
 
 
